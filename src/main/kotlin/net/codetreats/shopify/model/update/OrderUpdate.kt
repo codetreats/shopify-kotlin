87785cb14017ac14fa4b78933fa4937cb964641f
martin <github@msturm.net>
2026-09-14 13:41:32 +0200

    add transaction creation support and remove order status updates

--- src/main/kotlin/net/codetreats/shopify/model/update/OrderUpdate.kt
+++
@@ -1,8 +0,0 @@
-package net.codetreats.shopify.model.update
-
-import com.squareup.moshi.Json
-import net.codetreats.shopify.model.FinancialStatus
-
-data class OrderUpdate(
-        @Json(name = "financial_status") val financialStatus: FinancialStatus? = null,
-)
